ea99b0884c27b63dde4fbcaa928fbc50591eaae9
AxelSanchez1606 <axelsanchez066@gmail.com>
2025-06-12 21:03:09 -0300

    json de las pelis y series

---
+++ js/peliculasyseries.js
@@ -0,0 +1,347 @@
+[
+{
+    "imagen": "images/hulk_hero.png",
+    "titulo": "The incredible Hulk",
+    "clasificacion": 13,
+    "año": 2008,
+    "genero": [
+        "Acción",
+        "Fantasía"
+    ],
+    "descripcion": "En The Incredible Hulk, Bruce Banner se convierte en Hulk como un peón involuntario en un esquema militar para revitalizar el programa del supersoldado a través de radiación gamma.",
+    "resumen": "Bruce Banner recorre el mundo en busca de un antídoto para librarse de su alter ego. Además tendrá que hacer frente a Emil, un nuevo enemigo, lo que convertirá a Nueva York en el escenario de la última batalla entre las dos criaturas más poderosas.",
+    "trailer": "https://www.youtube.com/watch?v=xbqNb2PFKKA",
+    "elenco": [
+        "Edward Norton",
+        "Liv Tyler",
+        "Tim Roth",
+        "William Hurt",
+        "Tim Blake Nelson",
+        "Ty Burrell",
+        "Christina Cabot",
+        "Peter Mensah"
+    ],
+    "directores":[
+        "Louis Leterrier"
+    ],
+    "productores": [
+        "Avi Arad",
+        "Kevin Feige",
+        "Gale Anne Hurd",
+        "Stan Lee",
+        "David Maisel",
+        "Jim Van Wyck"
+
+    ],
+    "temporadas": 0,
+    "capitulos": 0
+},
+
+{
+    "imagen": "images/the-office_hero.jpg",
+    "titulo": "The Office",
+    "clasificacion": 13,
+    "año": 2005,
+    "genero": [
+        "Comedia"
+    ],
+    "descripcion": "The Office es una serie de televisión de comedia que sigue el día a día de los empleados de una sucursal de la empresa papelera ficticia",
+    "resumen": "The Office , popular serie de comedia de situación estadounidense que narra la vida cotidiana de un grupo de empleados de la ficticia empresa Dunder Mifflin Paper Company en Scranton , Pensilvania",
+    "trailer": "https://www.youtube.com/watch?v=vyq1FCeFrng",
+    "elenco": [
+        "Steve Carell",
+        "Rainn Wilson",
+        "John Krasinski",
+        "Jenna Fischer",
+        "B. J. Novak"
+    ],
+    "directores":[
+        "Amy Heckerling",
+        "Greg Daniels",
+        "Bryan Gordon",
+        "Ken Kwapis",
+        "Ken Whittingham"
+    ],
+    "productores": [
+        ""
+    ],
+    "temporadas": 9,
+    "capitulos": 201
+},
+
+{
+    "imagen": "images/shaolin-soccer_hero.png",
+    "titulo": "Shaolin soccer",
+    "clasificacion": 13,
+    "año": 2001,
+    "genero": [
+        "Acción",
+        "Comedia"
+    ],
+    "descripcion": "Sing es alumno del Shaolin y su verdadera ambición es popularizar y aproximar el arte del kung fu a la sociedad moderna.",
+    "resumen": "Un día conoce a Fung, un antiguo futbolista fracasado, ambos establecerán una buena relación de la que nacerá la idea de formar un equipo que combine el futbol con las artes marciales.",
+    "trailer": "https://www.youtube.com/watch?v=KEWUDWYDdYs",
+    "elenco": [
+        "Stephen Chow",
+        "Wei Zhao",
+        "Wong yat fei",
+        "Karen Mok",
+        "Cecilia Cheung",
+        "Danny Chan Kwok Kwan",
+        "Chiu Chi Ling",
+        "Zhang Ming Ming"
+    ],
+    "directores":[
+        "Stephen Chow"
+    ],
+    "productores": [
+        "Yeung Kwok Fai"
+    ],
+    "temporadas": 0,
+    "capitulos": 0
+},
+
+{
+    "imagen": "images/death-note_hero.jpg",
+    "titulo": "Death note",
+    "clasificacion": 14,
+    "año": 2006,
+    "genero": [
+        "Susupense",
+        "Fantasía"
+    ],
+    "descripcion":"Un libro malévolo cae en manos de Yagami Light. Ese libro tiene unas instrucciones claras y encierra una magia oscura.",
+    "resumen": "Yagami no lo cree en un principio pero una vez lo prueba y comprueba que es cierto, toda su vida cambia por completo. El protagonista usará este artilugio para eliminar del mundo todos esas personas malas, criminales y seres oscuros y malvados que lo acechan.",
+    "trailer": "https://www.youtube.com/watch?v=NlJZ-YgAt-c",
+    "elenco": [
+        "Mamoru Miyano",
+        "Shido Nakamura",
+        "Noriko Hidaka",
+        "Kappei Yamaguchi",
+        "Aya Hirano",
+        "Nozomu Sasaki",
+        "Naoya Uchida"
+    ],
+    "directores":[
+        "Tetsurō Araki"
+    ],
+    "productores": [
+        ""
+    ],
+    "temporadas": 1,
+    "capitulos": 37
+},
+
+{
+    "imagen": "/images/bad-boys_hero.jpg",
+    "titulo": "Bad Boys For Life",
+    "clasificacion": 17,
+    "año": 2020,
+    "genero": [
+        "Acción",
+        "Comedia"
+    ],
+    "descripcion": "Los detectives de Miami, Mike Lowrey y Marcus Burnett, se reúnen una última vez para acabar con un cartel de drogas que amenaza sus vidas.",
+    "resumen": "Mike y Marcus se ven obligados a trabajar juntos una vez más cuando un nuevo enemigo aparece en la escena, poniendo en peligro todo lo que han construido.",
+    "trailer": "https://www.youtube.com/watch?v=enizDUYZOCI",
+    "elenco": [
+        "Will Smith",
+        "Martin Lawrence",
+        "Vanessa Hudgens",
+        "Alexander Ludwig",
+        "Charles Melton",
+        "Paola Núñez",
+        "Nicky Jam",
+        "Kate del Castillo"
+    ],
+    "directores":[
+        "Adil El Arbi",
+        "Bilall Fallah"
+    ],
+    "productores": [
+        "Jerry Bruckheimer",
+        "Will Smith",
+        "Doug Belgrad",
+        "Chad Oman",
+        "Mike Stenson",
+        "James Lassiter",
+        "Barry H. Waldman"
+    ],
+    "temporadas": 0,
+    "capitulos": 0
+},
+
+{
+    "imagen": "images/squid-game_hero.jpg",
+    "titulo": "El juego del calamar",
+    "clasificacion": 16,
+    "año": 2021,
+    "genero": [
+        "Drama",
+        "Suspense"
+    ],
+    "descripcion": "Un grupo de personas con problemas financieros acepta una misteriosa invitación para participar en un juego de supervivencia con un premio en efectivo.",
+    "resumen": "A medida que los jugadores compiten en juegos infantiles con consecuencias mortales, deben enfrentarse a sus propios demonios y a la naturaleza humana.",
+    "trailer": "https://www.youtube.com/watch?v=oqxAJKy0ii4",
+    "elenco": [
+        "Jung-jae Lee",
+        "Lee Byung-hun",
+        "Wi Ha-Joon",
+        "Ha-Neul Kang",
+        "Sung-Hoon Park",
+        "Yu-ri Jo",
+        "Si-wan Yim",
+        "Yang Dong-Geun",
+        "Ae-sim Kang",
+        "Jin-wook Lee",
+        "Jae-won Roh",
+        "Ji-an Won",
+        "Park Gyu-young",
+        "Dai-Su Oh"
+    ],
+    "directores":[
+        "Hwang Dong-hyuk"
+    ],
+    "productores": [
+        "Hwang Dong-hyuk",
+        "Kim Ji-yeon"
+    ],
+    "temporadas": 3,
+    "capitulos": 17
+},
+
+{
+    "imagen": "/images/baila-vini_hero.jpg",
+    "titulo": "Baila, Vini",
+    "clasificacion": 0,
+    "año": 2025,
+    "genero": [
+        "Documental"
+    ],
+    "descripcion": "Un viaje a través de la historia de la música y la cultura del vinilo.",
+    "resumen": "Baila, Vini es un documental de Netflix sobre el futbolista brasileño Vinícius Júnior",
+    "trailer": "https://www.youtube.com/watch?v=IucwRVDHroc",
+    "elenco": [
+        "Vinicius Júnior",
+        "Neymar Jr.",
+        "Thiago Silva",
+        "Gabriel Jesus"
+    ],
+    "directores":[
+        "Andrucha Waddington",
+        "Emilio Domingos"
+    ],
+    "productores": [
+        "Luisa Barbosa",
+        "Renata Brandão"
+    ],
+    "temporadas": 0,
+    "capitulos": 0
+},
+
+{
+    "imagen": "images/breaking-bad_hero.jpg",
+    "titulo": "Breaking Bad",
+    "clasificacion": 14,
+    "año": 2008,
+    "genero": [
+        "Drama"
+    ],
+    "descripcion": "Un profesor de química se convierte en fabricante de metanfetaminas para asegurar el futuro financiero de su familia.",
+    "resumen": "Walter White, un profesor de química, se asocia con un exalumno para producir y vender metanfetaminas tras ser diagnosticado con cáncer.",
+    "trailer": "https://www.youtube.com/watch?v=HhesaQXLuRY",
+    "elenco": [
+        "Bryan Cranston",
+        "Aaron Paul",
+        "Anna Gunn",
+        "Dean Norris",
+        "Betsy Brandt",
+        "RJ Mitte",
+        "Bob Odenkirk",
+        "Jonathan Banks"
+    ],
+    "directores":[
+        "Vince Gilligan",
+        "Adam Bernstein",
+        "Michelle MacLaren",
+        "Michael Slovis",
+        "Peter Gould",
+        "Colin Bucksey",
+        "Thomas Schnauz",
+        "Sam Catlin",
+        "Rian Johnson",
+        "Bryan Cranston"
+    ],
+    "productores": [
+        "Karen Moore"
+    ],
+    "temporadas": 5,
+    "capitulos": 62
+},
+
+{
+    "imagen": "images/super-cool_hero.png",
+    "titulo": "Supercool",
+    "clasificacion": 18,
+    "año": 2007,
+    "genero": [
+        "Comedia",
+        "Adolescente"
+    ],
+    "descripcion": "Un adolescente intenta navegar por las complejidades de la vida, el amor y la amistad mientras enfrenta los desafíos de crecer.",
+    "resumen": "La historia sigue a un grupo de amigos mientras lidian con los altibajos de la adolescencia, explorando temas de identidad, pertenencia y el viaje hacia la adultez.",
+    "trailer": "https://www.youtube.com/watch?v=au2Zq8D9RaY",
+    "elenco": [
+        "Michael Cera",
+        "Christopher Mintz-Plasse",
+        "Jonah Hill",
+        "Emma Stone",
+    ],
+    "directores":[
+        "Greg Mottola"
+    ],
+    "productores": [
+        "Evan Goldberg",
+        "Greg Mottola",
+    ],
+    "temporadas": 0,
+    "capitulos": 0
+},
+
+{
+    "imagen": "images/eternauta_hero.jpg",
+    "titulo": "El eternauta",
+    "clasificacion": 14,
+    "año": 2025,
+    "genero": [
+        "Drama",
+        "Ciencia Ficción"
+    ],
+    "descripcion": "Un grupo de amigos se enfrenta a una invasión alienígena mientras intentan sobrevivir en un mundo post-apocalíptico.",
+    "resumen": "El eternauta es una historia de ciencia ficción que explora la lucha de la humanidad contra fuerzas externas y la importancia de la amistad y la solidaridad.",
+    "trailer": "https://www.youtube.com/watch?v=ykLTd5aTa88",
+    "elenco": [
+        "Ricardo Darín",
+        "Carla Peterson",
+        "Cesar Troncoso",
+        "Andrea Pietra",
+        "Ariel Staltari",
+        "Marcelo Subiotto",
+        "Claudio Martínez Bel",
+        "Orianna Cardenas",
+        "Mora Fisz"
+    ],
+    "directores":[
+        "Bruno Stagnaro"
+    ],
+    "productores": [
+        "Micaela Buye",
+        "Hugo Sigman",
+        "Matias Mosteirín",
+        "Leticia Cristi",
+        "Diego Copello"
+    ],
+    "temporadas": 1,
+    "capitulos": 6
+}
+]
