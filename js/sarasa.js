d8116a40d8cc02366db142339e3886b673a270dc
NicolasGeo92 <nfg.1992@gmail.com>
2025-06-13 00:44:44 -0300

    me voy a quedar pelado

---
+++ js/sarasa.js
@@ -0,0 +1,30 @@
+//BORRE LOS ID´S DEL INPUT DEL INPUT RADIO Y INGRESO DE NUMERO.
+//BORRE LOS ID´S DEL INPUT DE CONTRASEÑA Y REPETIR CONTRASEÑA POR QUE ESTAN REPETIDOS
+
+// - SOLO 3 DIGITOS NI MAS NI MENOS
+// - SOLAMENTE NUMEROS
+// - QUE NO SEA 0 NI 000
+// - QUE NO TENGA ESPACIOS
+
+const expresiones = {
+    claveTarjeta: /^\d{3}$/
+}
+
+function validacionClaveDeLaTarjeta() {
+    const claveErronea = "000";
+    const claveErroneaEntero = parseInt(claveErronea);
+
+    const claveDeLaTarjeta = document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad");
+
+    claveDeLaTarjeta.addEventListener("keypress", function() {
+        if (expresiones.claveTarjeta.test(claveDeLaTarjeta.value)){
+            document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad").style.backgroundColor = "red";
+        } else {
+            document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad").style.backgroundColor = "blue";
+        }
+    });
+
+    const claveIngresada = parseInt(claveDeLaTarjeta.value);
+}
+
+validacionClaveDeLaTarjeta();
