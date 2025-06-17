ec2245314c1e681d3d5e4268029d46329efe7273
NicolasGeo92 <nfg.1992@gmail.com>
2025-06-17 00:53:50 -0300

    Numero valido terminado

--- js/sarasa.js
+++ js/sarasa.js
@@ -9,28 +9,66 @@
 // - SOLAMENTE NUMEROS
 // - QUE NO SEA 0 NI 000
 // - QUE NO TENGA ESPACIOS
-// - TIENE QUE SER REQUERIDO UNO DE LOS TRES METODOS DE PAGO
+// - TIENE QUE SER REQUERIDO UNO DE LOS TRES METODOS DE PAGO (CREO)
 
 const expresiones = {
     numeroDeTarjeta: /^\d{16}$/,
     claveTarjeta: /^\d{3}$/
 }
 
-const inputs = document.querySelectorAll(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad");
+const inputs = document.querySelectorAll(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad, .metodoDePago__tarjetaDeDebitoOCredito__numeroDeTarjeta");
 console.log(inputs);
 
-// function validacionNumeroDeTarjeta() {
+function longitudNumeroDeTarjeta() {
+    let numeroDeTarjeta = document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__numeroDeTarjeta").value;
+    const longitudValida = expresiones.numeroDeTarjeta.test(numeroDeTarjeta);
 
-// }
+    if (longitudValida == true) {
+        return numeroDeTarjeta;
+    } return false;
+}
+
+function numeroDeTarjetaValido() {
+    const numeroDeTarjeta = longitudNumeroDeTarjeta();
+    let vector = parseInt(numeroDeTarjeta, 10);
+    let resultado = 0;
+
+    for (let i = 0; i < numeroDeTarjeta.length - 1; i++) {
+        vector = parseInt(numeroDeTarjeta[i], 10);
+        resultado += vector;
+    }
+
+    let resultadoPar = resultado % 2;
+    let ultimoValorDeLaTarjeta = numeroDeTarjeta[15] % 2;
+
+    if (resultadoPar === 0 && ultimoValorDeLaTarjeta === 1) {
+        return true;
+    } else if (resultadoPar === 1 && ultimoValorDeLaTarjeta === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function validacionNumeroDeTarjeta() {
+    if (numeroDeTarjetaValido() == true) {
+        document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__numeroDeTarjeta").classList.remove("validacion-incorrecta");
+        document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__numeroDeTarjeta").classList.add("validacion-correcta");
+        document.querySelector(".numeroDeTarjeta-incorrecto").style.display = "none";
+    } else {
+        document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__numeroDeTarjeta").classList.add("validacion-incorrecta");
+        document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__numeroDeTarjeta").classList.remove("validacion-correcta");
+        document.querySelector(".numeroDeTarjeta-incorrecto").style.display = "flex";
+    };
+}
 
 function validacionClaveDeLaTarjeta() {
-    const claveErronea = "000";
-    const claveErroneaEntero = parseInt(claveErronea);
-
+    const claveErronea = 0;
     const claveDeLaTarjeta = document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad").value;
     const esValido = expresiones.claveTarjeta.test(claveDeLaTarjeta);
 
-    if (esValido == true && claveDeLaTarjeta != claveErroneaEntero) {
+    if (esValido == true && claveDeLaTarjeta != claveErronea) {
         document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad").classList.remove("validacion-incorrecta");
         document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad").classList.add("validacion-correcta");
         document.querySelector(".codigoDeSeguridad-incorrecto").style.display = "none";
@@ -41,17 +79,11 @@ function validacionClaveDeLaTarjeta() {
     };
 }
 
-// function mensajeInvalido() {
-//     let parrafo = document.createElement("p");
-//     let mensajeDeError = document.createTextNode("Clave incorrecta");
-//     parrafo.appendChild(mensajeDeError);
-//     document.querySelector(".metodoDePago").appendChild(parrafo);
-// }
-
 function validacionIncorrectaInputs() {
     inputs.forEach((input) => {
         input.addEventListener("keyup", function () {
             validacionClaveDeLaTarjeta();
+            validacionNumeroDeTarjeta();
         });
     });
 }
@@ -60,11 +92,21 @@ function validacion() {
     inputs.forEach((input) => {
         input.addEventListener("blur", function () {
             validacionClaveDeLaTarjeta();
+            validacionNumeroDeTarjeta();
             document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad").classList.remove("validacion-correcta");
             document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad").classList.remove("validacion-incorrecta");
+            document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__numeroDeTarjeta").classList.remove("validacion-correcta");
+            document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__numeroDeTarjeta").classList.remove("validacion-incorrecta");
         });
     });
 }
+
+// function mensajeInvalido() {
+//     let parrafo = document.createElement("p");
+//     let mensajeDeError = document.createTextNode("Clave incorrecta");
+//     parrafo.appendChild(mensajeDeError);
+//     document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito").appendChild(parrafo);
+// }
 
 validacionIncorrectaInputs();
 validacion();
