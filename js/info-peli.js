80c6d69ffb22a7713392f13aafba6ac79afc591e
AxelSanchez1606 <axelsanchez066@gmail.com>
2025-06-19 00:51:47 -0300

    carrusel (a medias jaja)

--- js/info-peli.js
+++ js/info-peli.js
@@ -18,6 +18,7 @@ if(tituloPeli){
     document.getElementById("generos-detallado").textContent = tituloPeli.genero;
     document.getElementById("año-detallado").textContent = tituloPeli.año;
     document.getElementById("iframe").src = tituloPeli.iframe;
+    document.getElementById("iframe-movil").src = tituloPeli.iframe;
     if(tituloPeli.clasificacion !=0){
         document.getElementById("clasificacion-detallado").textContent = tituloPeli.clasificacion;
     }
@@ -26,3 +27,36 @@ if(tituloPeli){
     document.getElementById("productores").textContent = tituloPeli.productores;
 }
 
+function App(){}
+    window.onload = function(event){
+        var app = new App();
+        window.app = app;
+
+    }
+
+    App.prototype.processingButton = function(event){
+        const btn = event.currentTarget;
+        const carruselList = event.currentTarget.parentNode;
+        track = event.currentTarget.parentNode.querySelector("#track");
+        const carrusel = track.querySelectorAll(".carrusel");
+        
+        const carruselWidth = carrusel[0].offsetWidth;
+        
+        const trackWidth = track.offsetWidth;
+        const listWidth = carruselList.offsetWidth;
+
+        track.style.left == "" ? leftPosition = track.style.left = 0: leftPosition = parseFloat(track.style.left.slice(0,-2) * -1);
+        btn.dataset.button == "button-prev" ? prevAction(leftPosition, carruselWidth, track) : nextAction(leftPosition, trackWidth, listWidth, carruselWidth, track);
+    }
+
+    let prevAction = (leftPosition, carruselWidth, track) => {
+        if(leftPosition > 0){
+            track.style.left = `${-1 * (leftPosition -carruselWidth)}px`;
+        }
+    }
+
+    let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) =>{
+        if(leftPosition < (trackWidth - listWidth)){
+            track.style.left = `${-1 * (leftPosition + carruselWidth)}px`;
+        }
+    }
