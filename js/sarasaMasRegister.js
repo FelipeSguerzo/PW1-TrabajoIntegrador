938067358409f9b20af4ffc3d94c9c8ee519f3ad
NicolasGeo92 <nfg.1992@gmail.com>
2025-06-22 04:06:19 -0300

    Juntando los js

--- js/sarasaMasRegister.js
+++ js/sarasaMasRegister.js
@@ -1,55 +1,112 @@
-// AGREGUE CLASS A LOS INPUT´S DEL .DATOSDEUSUARIO. (HTML) Y MODIFIQUE LOS SELECTORES EN EL CSS, 
-// LO LLAME DE MANERA DIRECTA (.FORM-DATOS), NO DE SELECTOR A SELECTOR, ANTES LO LLAMABA .FORMULARIO .DATOSDEUSUARIO INPUT.
-// AL PARECER LOS EVENTOS DE "SUBMIT" SE ESTAN PISANDO EN LOS DOS ARCHIVOS JS.
-// PREGUNTARLE A FELI SI SACAMOS LOS REQUIRED.
+// REVISAR CON CUPONES DE PAGO COMO HACER PARA QUE LO GUARDE EN EL LOCALSTORAGE
 
-// Agregue la varialbe let claveCorrecta; a la funcion validacionClaveDeLaTarjeta();
+const nodoContrasenia = document.querySelector("#contrasenia")
+const nodoRepetirContrasenia = document.querySelector("#repetirContrasenia")
+const nodoUsername = document.querySelector("#nombreDeUsuario")
+const nodoNumeroTarjeta = document.querySelector("#tarjetaDeDebitoOCredito")
+const buttonConfirm = document.querySelector("#confirm");
+const form = document.querySelector("form");
+const mensajeContrasenia = document.querySelector(".mensaje-contrasenia");
+const mensajeRepetir = document.querySelector(".mensaje-repetir")
+const mensajeUsername = document.querySelector(".mensaje-usuario")
 
+//--------------------------------------------------------NICO--------------------------------------------------------------
 const formulario = document.querySelector(".formulario");
 const inputs = document.querySelectorAll(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad, .metodoDePago__tarjetaDeDebitoOCredito__numeroDeTarjeta");
 
-let radioTarjeta = document.getElementById("radioTarjeta");
-let radioCupon = document.getElementById("cuponDePagoRadio");
-let radioTransferencia = document.getElementById("transferenciaBancaria");
+const radioTarjeta = document.getElementById("radioTarjeta");
+const radioCupon = document.getElementById("cuponDePagoRadio");
+const radioTransferencia = document.getElementById("transferenciaBancaria");
 const metodoDePagoTarjeta = document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__numeroDeTarjeta");
-let metodoDePagoClaveTarjeta = document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad");
-let metodoDePagoPagoFacil = document.getElementById("pagoFacil");
-let metodoDePagoRapiPago = document.getElementById("rapiPago");
+const metodoDePagoClaveTarjeta = document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad");
+const metodoDePagoPagoFacil = document.getElementById("pagoFacil");
+const metodoDePagoRapiPago = document.getElementById("rapiPago");
 const mensajeDeErrorNumeroInvalido = document.querySelector(".numeroDeTarjeta-incorrecto");
 const mensajeDeErrorClaveInvalida = document.querySelector(".codigoDeSeguridad-incorrecto");
-let mensajeDeErrorCupon = document.querySelector(".cupon-incorrecto");
-let mensajeFormularioInvalido = document.querySelector(".formulario-invalido");
+const mensajeDeErrorCupon = document.querySelector(".cupon-incorrecto");
+const mensajeFormularioInvalido = document.querySelector(".formulario-invalido");
 
 const expresiones = {
     numeroDeTarjeta: /^\d{16}$/,
     claveTarjeta: /^\d{3}$/
 }
 
-formulario.addEventListener("submit", function (event) {
-    event.preventDefault();
+//--------------------------------------------------------NICO--------------------------------------------------------------
 
-    // const radios = document.querySelectorAll(".radio");
-    // const transferencia = document.getElementById("transferenciaBancaria");
-    let formularioValido = true;
+console.log("a");
 
-    if (!numeroDeTarjetaValido() || !validacionClaveDeLaTarjeta()) {
-        formularioValido = false;
-        mensajeFormularioInvalido.textContent = "Complete todos los campos";
-    } else {
-        formulario.submit();
+const getUsuarios = () => {
+    const usuarios = localStorage.getItem("usuarios");
+    return usuarios ? JSON.parse(usuarios) : [];
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    mensajeRepetir.textContent = "";
+    mensajeContrasenia.textContent = "";
+    const usuario = nodoUsername.value;
+    const contrasenia = nodoContrasenia.value
+    const contraseniaRepetida = nodoRepetirContrasenia.value;
+    const numeroDeTarjetaDelUsuario = metodoDePagoTarjeta.value
+    const claveDeTarjetaDelUsuario = metodoDePagoClaveTarjeta.value;
+
+    if (!verificarContrasenia(contrasenia)) {
+        mensajeContrasenia.textContent = "La contraseña debe tener al menos 2 letras, 2 numeros y 2 caracteres especiales";
+        mensajeContrasenia.style.color = "red";
+        return
     }
 
-    if (!cuponDePago()) {
-        formularioValido = false;
-    } else {
-        formulario.submit();
+    if (contrasenia !== contraseniaRepetida) {
+        mensajeRepetir.textContent = "Las contraseñas deben ser iguales";
+        mensajeRepetir.style.color = "red";
+        return
     }
 
-    if (radioTransferencia.checked == true) {
-        formulario.submit();
+    if (crearUsuario(usuario, contrasenia) && (radioTransferencia.checked == true || cuponDePago() == true || ((numeroDeTarjetaValido(numeroDeTarjetaDelUsuario) == true) && validacionClaveDeLaTarjeta(claveDeTarjetaDelUsuario) == true))) {
+        form.submit();
     }
-
 })
+
+function crearUsuario(username, contrasenia) {
+    const usuarios = getUsuarios();
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].username === username) {
+            mensajeUsername.textContent = "Ya hay un usuario registrado con este nombre. Pruebe otro"
+            mensajeUsername.style.color = "red";
+            return false;
+        }
+    }
+
+    usuarios.push({ username, contrasenia });
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    console.log("La cuenta se ha creado con exito");
+    return true;
+}
+
+
+function verificarContrasenia(contrasenia) {
+    const letras = contrasenia.match(/[a-zA-Z]/g) || [];
+    const numeros = contrasenia.match(/[0-9]/g) || [];
+    const caracteresEspeciales = contrasenia.match(/[!"#$%&/]/g) || [];
+
+    return letras.length >= 2 && numeros.length >= 2 && caracteresEspeciales.length >= 2;
+}
+
+
+function eliminarUsuario(username) {
+    const usuarios = getUsuarios();
+    const usuariosANoEliminar = []
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].username !== username) {
+            usuariosANoEliminar.push(usuarios[i]);
+        }
+    }
+
+    localStorage.setItem("usuarios", JSON.stringify(usuariosANoEliminar));
+}
+
+//--------------------------------------------------------NICO--------------------------------------------------------------
 
 //INPUTS DE LOS METODOS DE PAGO DESACTIVADOS.
 
@@ -96,49 +153,41 @@ function habilitarTransferencia() {
 
 //INPUTS DE LOS METODOS DE PAGO DESACTIVADOS.
 
-// VALIDACION NUMERO DE TARJETA
-
-function longitudNumeroDeTarjeta() {
-    let numeroDeTarjetaIngresada = document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__numeroDeTarjeta").value;
+function numeroDeTarjetaValido(numeroDeTarjetaIngresada) {
     const longitudValida = expresiones.numeroDeTarjeta.test(numeroDeTarjetaIngresada);
 
-    if (longitudValida == false) {
-        return false;
-    } else {
-        return numeroDeTarjetaIngresada;
-    }
-}
-
-function numeroDeTarjetaValido() {
-    const numeroDeTarjeta = longitudNumeroDeTarjeta();
+    const numeroDeTarjeta = numeroDeTarjetaIngresada;
     let vector = parseInt(numeroDeTarjeta, 10);
     let resultado = 0;
     let resultadoPar;
     let ultimoValorDeLaTarjeta;
-    let tarjetaValida = true;
+    let tarjetaValida = false;
 
-    for (let i = 0; i < numeroDeTarjeta.length - 1; i++) {
-        vector = parseInt(numeroDeTarjeta[i], 10);
-        resultado += vector;
+    if (longitudValida == true) {
+        for (let i = 0; i < numeroDeTarjeta.length - 1; i++) {
+            vector = parseInt(numeroDeTarjeta[i], 10);
+            resultado += vector;
+        }
+
+        resultadoPar = resultado % 2;
+        ultimoValorDeLaTarjeta = numeroDeTarjeta[15] % 2;
+
+        if (resultadoPar === 0 && ultimoValorDeLaTarjeta === 1) {
+            tarjetaValida = true;
+        } else if (resultadoPar === 1 && ultimoValorDeLaTarjeta === 0) {
+            tarjetaValida = true;
+        } else {
+            tarjetaValida = false;
+        }
     }
-
-    resultadoPar = resultado % 2;
-    ultimoValorDeLaTarjeta = numeroDeTarjeta[15] % 2;
-
-    if (resultadoPar === 0 && ultimoValorDeLaTarjeta === 1) {
-        tarjetaValida = true;
-    } else if (resultadoPar === 1 && ultimoValorDeLaTarjeta === 0) {
-        tarjetaValida = true;
-    } else {
-        tarjetaValida = false;
-    }
-
     return tarjetaValida;
 }
 
 function visualizacionNumeroDeTarjeta() {
-    document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__numeroDeTarjeta").addEventListener("keyup", function () {
-        if (numeroDeTarjetaValido() == false) {
+    metodoDePagoTarjeta.addEventListener("keyup", function () {
+        const numero = this.value;
+
+        if (numeroDeTarjetaValido(numero) == false) {
             metodoDePagoTarjeta.classList.add("validacion-incorrecta");
             metodoDePagoTarjeta.classList.remove("validacion-correcta");
             document.querySelector(".numeroDeTarjeta-incorrecto").style.display = "flex";
@@ -155,14 +204,13 @@ function visualizacionNumeroDeTarjeta() {
 
 // VALIDACION CLAVE DE LA TARJETA
 
-function validacionClaveDeLaTarjeta() {
+function validacionClaveDeLaTarjeta(claveIngresada) {
     const claveErronea = 0;
-    const claveDeLaTarjeta = document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad").value;
-    const esValido = expresiones.claveTarjeta.test(claveDeLaTarjeta);
+    const esValido = expresiones.claveTarjeta.test(claveIngresada);
     let claveCorrecta = true;
 
-    if (esValido == true && claveDeLaTarjeta != claveErronea) {
-        claveCorrecta;
+    if (esValido == true && claveIngresada != claveErronea) {
+        claveCorrecta = true;
     } else {
         claveCorrecta = false;
     };
@@ -171,8 +219,10 @@ function validacionClaveDeLaTarjeta() {
 }
 
 function visualizacionClaveDeLaTarjeta() {
-    document.querySelector(".metodoDePago__tarjetaDeDebitoOCredito__codigoDeSeguridad").addEventListener("keyup", function () {
-        if (validacionClaveDeLaTarjeta() == true) {
+    metodoDePagoClaveTarjeta.addEventListener("keyup", function () {
+        const clave = this.value;
+
+        if (validacionClaveDeLaTarjeta(clave) == true) {
             metodoDePagoClaveTarjeta.classList.remove("validacion-incorrecta");
             metodoDePagoClaveTarjeta.classList.add("validacion-correcta");
             document.querySelector(".codigoDeSeguridad-incorrecto").style.display = "none";
@@ -250,3 +300,5 @@ validacion();
 habilitarTarjeta();
 habilitarCuponDePago();
 habilitarTransferencia();
+
+//--------------------------------------------------------NICO--------------------------------------------------------------
