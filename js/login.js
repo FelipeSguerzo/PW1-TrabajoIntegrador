284c7ca75acc41e55086caeff0da9a2f398acfc8
FelipeSguerzo <fsguerzo231@alumno.unlam.edu.ar>
2025-06-22 19:32:33 -0300

    Iniciar sesion

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 const botonIniciarSesion = document.querySelector("button.login");
 const inputUsername = document.querySelector("#usuario");
 const inputPassword = document.querySelector("#password");
+const form = document.querySelector("form");
+
 
 function validarInputs(){
     if(inputPassword.value.trim() === "" || inputUsername.value.trim() === ""){
@@ -14,3 +16,65 @@ inputUsername.addEventListener("input",validarInputs)
 inputPassword.addEventListener("input",validarInputs)
 
 validarInputs();
+
+const getUsuarios = () => {
+    const usuarios = localStorage.getItem("usuarios");
+    return usuarios ? JSON.parse(usuarios) : [];
+}
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const usernameErrorMessage = document.querySelector(".username-error");
+    const passwordErrorMessage = document.querySelector(".password-error");
+    usernameErrorMessage.textContent = "";
+    passwordErrorMessage.textContent = "";
+    
+    const username = inputUsername.value;
+    const contrasenia = inputPassword.value;
+    
+    if(!usuarioRegistrado(username)){
+        usernameErrorMessage.textContent ="No hay ningun usuario registrado con ese nombre"
+        usernameErrorMessage.style.color = "red";
+        return
+    }
+
+    if(!coincideContraseniaConUsuario(username,contrasenia)){
+        passwordErrorMessage.textContent = "La contraseña es incorrecta"
+        passwordErrorMessage.style.color = "red";
+        return
+    }
+
+    const usuarioSesionIniciada = getUsuarioSesionIniciada();
+    usuarioSesionIniciada.push({username,contrasenia});
+    localStorage.setItem("usuarioSesionIniciada",JSON.stringify(usuarioSesionIniciada));
+    form.submit();
+})
+
+const getUsuarioSesionIniciada = ()=>{
+    const usuario = localStorage.getItem("usuarioSesionIniciada");
+    return usuario ? JSON.parse(usuario) : [];
+}
+
+function usuarioRegistrado(username){
+    const usuarios = getUsuarios();
+    let seEncontro = false;
+    usuarios.forEach((element)=>{
+        if(element.username===username){
+            seEncontro = true;
+        }
+    })
+
+    return seEncontro;
+}
+
+function coincideContraseniaConUsuario(username,contrasenia){
+    const usuarios = getUsuarios();
+    let coinciden = false;
+    usuarios.forEach((element)=>{
+        if(element.username===username && element.contrasenia === contrasenia){
+            coinciden = true;
+        }
+    })
+
+    return coinciden;
+}
